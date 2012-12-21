dd749331cf870362b13c49a96a636a663c1a8066
Chris U <chris.uyehara@va.gov>
2012-12-21 18:45:08 -0500

    Fixed build script; repackaged T6

--- Packages/Medical Domain Web Services/Routines/MDWSPKG.m
+++ Packages/Medical Domain Web Services/Routines/MDWSPKG.m
@@ -1,6 +1,5 @@
 MDWSPKG ;MTZ/CKU - PACKAGING TOOLS; 12/19/2012
- ;;1.0;MDWS;**1**;Dec 19, 2012;
- Q
+ ;;1.0;MDWS;**1**;Dec 21, 2012;
 EN
  ; This sub-routine will allow a programmer to import routines from a Git repo.
  ; It is made for the MDWS team to quickly absorb the changes made without 
@@ -10,8 +9,10 @@ EN
  ; routines which were originally included in the last build and import them from
  ; the Git repo.
  N DIC S DIC="^XPD(9.6," S DIC(0)="AE" D ^DIC Q:Y<0
- N BUILDIEN S BUILDIEN=+Y W !
- N RESULT,ERRORS D LIST^DIC(9.68,",9.8,"_BUILDIEN_",","@;.01","P",,,,,,,"RESULT","ERRORS")
+ N BUILDIEN S BUILDIEN=+Y 
+ N % S %=2 W !,"Continue using BUILD: "_$P(Y,U,2)_" " D YN^DICN I %=-1 Q
+ I (%=2)!(%=0) G EN
+ W ! N RESULT,ERRORS D LIST^DIC(9.68,",9.8,"_BUILDIEN_",","@;.01","P",,,,,,,"RESULT","ERRORS")
  I $D(ERRORS) ZW ERRORS Q
  N I S I=0 F  S I=$O(RESULT("DILIST",I)) Q:I=""  D
  . N ROUTINE,X S (ROUTINE,X)=$P(RESULT("DILIST",I,0),U,2)
